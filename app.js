20391401561129e30e79ceed0e2a21896576996b
Vaibhav Gupta <official.guptavaibhav@gmail.com>
2022-10-15 17:05:14 +0530

    Fourth Commit

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require('express');
 const users = require('./models/users');
 require('./connect/dbconfig');
 const products = require('./models/products');
+const jwt = require('jsonwebtoken');
 const app = express();
 dotenv.config();
+const auth = require('./auth');
 
 
 app.use(express.json());
@@ -19,7 +21,7 @@ app.post('/register', async (req, res) => {
 })
 
 // User Login API
-app.post('/login', async (req, res) => {
+app.post('/login', auth,  async (req, res) => {
     if (req.body.password && req.body.email) {
         let user = await users.findOne(req.body).select('-password');
         if (user) {
@@ -39,13 +41,12 @@ app.post('/addProduct', async (req, res) => {
     res.send(result);
 })
 
-
 // Fetch Single Product
-app.get('/product/:id',async(req, res)=>{
-    let result = await products.findOne({_id: req.params.id});
-    if(result){
+app.get('/product/:id', async (req, res) => {
+    let result = await products.findOne({ _id: req.params.id });
+    if (result) {
         res.send(result);
-    }else{
+    } else {
         res.send("No Product Found");
     }
 })
@@ -60,11 +61,47 @@ app.get('/products', async (req, res) => {
     }
 })
 
-
 // Delete Product API
 app.delete('/deleteProduct/:id', async (req, res) => {
     let result = await products.deleteOne({ _id: req.params.id })
     res.send(result)
 })
+
+// Update Product API
+app.put('/updateProduct/:id', async (req, res) => {
+    let result = await products.updateOne({ _id: req.params.id }, { $set: req.body });
+    res.send(result);
+})
+
+// Search Product API
+app.get('/search/:key', async(req, res)=>{
+    let result = await products.find({
+        "$or":[
+            {
+                name : { $regex: req.params.key }
+            },
+            {
+                company : { $regex: req.params.key }
+            },
+            {
+                category : { $regex: req.params.key }
+            }
+        ]
+    })
+    res.send(result);
+})
+
+const createtoken = async()=>{
+    const token = await jwt.sign({ _id:"634a91ec2de432cc42aca435"}, "task_app", {
+        expiresIn : "1 hr"
+    });
+    console.log(token);
+
+    const userVer = await jwt.verify(token, "task_app");
+    console.log(userVer);
+}
+createtoken();
+
+
 app.listen(process.env.PORT || 5000);
 
--- models/users.js
+++ models/users.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     name : String,
     email: String,
     password : String
+});
+
+userSchema.pre('save', async function (next){
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
 });
 
 module.exports = mongoose.model('users', userSchema);
